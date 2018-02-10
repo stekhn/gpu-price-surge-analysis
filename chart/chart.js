001b8e17af3be5927a77473532f7f12eb1d44407
Steffen Kühne <stekhn@gmail.com>
2018-02-10 20:02:25 +0100

    Add responsive resize behaviour

--- chart/chart.js
+++ chart/chart.js
@@ -1,9 +1,12 @@
 document.addEventListener('DOMContentLoaded', init, false);
 
 var width = 800;
-var height = 400;
+var height = 500;
 
-var margin = { top: 10, right: 45, bottom: 20, left: 45 };
+var margin = { top: 40, right: 60, bottom: 40, left: 60 };
+
+var timeout;
+var gpuData, currencyData;
 
 function init() {
 
@@ -12,24 +15,60 @@ function init() {
   d3.json('data/gpu-percentages.json', function (data) {
 
     data.push({ key: 'average', value: getAverage(data) });
-    var scale = drawAxis(data, chart, true);
-    drawLines(data, chart, scale, 'dodgerblue');
+    gpuData = data;
+
+    drawGpus(data, chart);
   });
 
   d3.json('data/currency-percentages.json', function (data) {
 
     data.push({ key: 'average', value: getAverage(data) });
-    var scale = drawAxis(data, chart, false);
-    drawLines(data, chart, scale, 'tomato');
+    currencyData = data;
+
+    drawCurrencies(data, chart);
   });
+
+  window.addEventListener('resize', redraw, false);
+}
+
+function drawGpus(data, chart) {
+
+  var current = {};
+
+  current.data = data || gpuData;
+  current.text = 'GPUs';
+  current.color = 'dodgerblue';
+  current.drawAxis = true;
+  current.chart = chart;
+  current.scale = drawAxis(current);
+  current.lines = drawLines(current);
+  current.marker = drawMarker(current);
+}
+
+function drawCurrencies(data, chart) {
+
+  var current = {};
+
+  current.data = data || currencyData;
+  current.text = 'Cyptocurrencies';
+  current.color = 'tomato';
+  current.drawAxis = false;
+  current.chart = chart;
+  current.scale = drawAxis(current);
+  current.lines = drawLines(current);
+  current.marker = drawMarker(current);
 }
 
 function drawChart() {
 
-  var svg = d3.select('#chart')
+  var container = d3.select('#chart');
+
+  width = container.node().getBoundingClientRect().width;
+
+  var svg = container
     .append('svg')
-      .attr('width', width + margin.left + margin.right)
-      .attr('height', height + margin.top + margin.bottom);
+      .attr('width', width)
+      .attr('height', height);
 
   var chart = svg.append('g')
     .attr('transform', 'translate(' + margin.left + ',' + margin.top + ')');
@@ -37,9 +76,9 @@ function drawChart() {
   return chart;
 }
 
-function drawAxis(data, chart, draw) {
+function drawAxis(current) {
 
-  var xMax = d3.max(data, function (d) {
+  var xMax = d3.max(current.data, function (d) {
     return d3.max(d.value, function (c) {
       return new Date(c.date);
     });
@@ -47,16 +86,17 @@ function drawAxis(data, chart, draw) {
 
   var xScale = d3.scaleTime()
     .domain([new Date('2017-08-09'), xMax])
-    .range([0, width]);
+    .range([0, width - margin.left -margin.right]);
 
   var yScale = d3.scaleLinear()
     .domain([-100, 100])
-    .range([height, 0]);
+    .range([height - margin.top -margin.bottom, 0]);
 
-  if (draw) {
+  if (current.drawAxis) {
 
-    chart.append('g')
-        .attr('transform', 'translate(0,' + height + ')')
+    current.chart.append('g')
+        .attr('class', 'axis')
+        .attr('transform', 'translate(0,' + (height - margin.top - margin.bottom) + ')')
         .call(d3.axisBottom(xScale).ticks(5))
       .append('text')
         .attr('x', width)
@@ -65,8 +105,11 @@ function drawAxis(data, chart, draw) {
         .style('text-anchor', 'end')
         .text('');
 
-    chart.append('g')
-        .call(d3.axisLeft(yScale).ticks(5))
+    current.chart.append('g')
+        .attr('class', 'axis')
+        .call(d3.axisLeft(yScale).ticks(5).tickFormat(function (d) {
+          return (d <= 0 ? '' : '+') + d + ' %';
+        }))
       .append('text')
         .attr('x', 0)
         .attr('y', 20)
@@ -82,19 +125,23 @@ function drawAxis(data, chart, draw) {
   };
 }
 
-function drawLines(data, chart, scale, color) {
+function drawLines(current) {
 
   var line = d3.line()
-    .x(function (d) { return scale.x(new Date(d.date)); })
-    .y(function (d) { return scale.y(d.price); })
+    .x(function (d) {
+      return current.scale.x(new Date(d.date));
+    })
+    .y(function (d) {
+      return current.scale.y(d.price);
+    })
     .curve(d3.curveCardinal);
 
-  var lines = chart.selectAll('.line')
-      .data(data)
+  var lines = current.chart.selectAll('.line')
+      .data(current.data)
       .enter()
     .append('path')
       .attr('fill', 'none')
-      .attr('stroke', color)
+      .attr('stroke', current.color)
       .attr('stroke-width', function (d) {
         return d.key == 'average' ? 3 : 1.5;
       })
@@ -107,6 +154,42 @@ function drawLines(data, chart, scale, color) {
       });
 
   return lines;
+}
+
+function drawMarker(current) {
+
+  var lastValue = current.data.filter(function (d) {
+    return d.key == 'average'; }
+  )[0].value[0];
+
+  var marker = current.chart.append('g')
+    .attr('transform', function () {
+      return 'translate(' + current.scale.x(new Date(lastValue.date)) + ',' +
+        current.scale.y(lastValue.price) +')';
+    });
+
+  marker.append('text')
+    .attr('dx', '.5em')
+    .attr('dy', '.25em')
+    .attr('fill', current.color)
+    .attr('text-anchor', 'start')
+    .text('+' + Math.round(lastValue.price) + ' %');
+}
+
+function redraw () {
+
+  clearTimeout(timeout);
+
+  timeout = setTimeout(function () {
+
+    d3.select('#chart').html('');
+
+    var chart = drawChart();
+
+    drawGpus(undefined, chart);
+    drawCurrencies(undefined, chart);
+
+  }, 500);
 }
 
 function getAverage(data) {
